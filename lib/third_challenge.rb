8c0b2c56c5ed38642fec5f07d80054a7d07a6bd7
John Karrell Speck <speckworks@gmail.com>
2019-08-19 19:58:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,8 +8,6 @@ def third_challenge
       :hero => {[:name => "Romeo", :age =>"15"] :status => "alive"}}
    :capulet => {
       :patriarch => {:name => "Lord Capulet", :age =>"53"},
-      :matriarch => {:name => "Lady Capulet"
-        #your key/value pairs here
-        },
+      :matriarch => {:name => "Lady Capulet", :age => "51"},
       :heroine => {:name => "Juliet", :age => "15", :status =>"alive"}}
 end
