794bbb156e1e28baa357a89d255056eb3a6d766d
John Karrell Speck <speckworks@gmail.com>
2019-08-19 19:55:18 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,10 +1,11 @@
+require "pry"
 def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {:name => "Lord Montague", :age => "53"},
         #your key/value pairs here
       :matriarch => {:name => "Lady Montague", :age => "54"},
-        #your key/value pairs here
+binding.pry
       :hero => {[:name => "Romeo", :age =>"15"] :status => "alive"},
    :capulet => {
       :patriarch => {:name => "Lord Capulet", :age =>"53"},
