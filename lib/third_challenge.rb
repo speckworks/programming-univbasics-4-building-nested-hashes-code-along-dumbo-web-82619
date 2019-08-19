4e5a2ac397e5cc531d9c995da5554e207730eff7
John Karrell Speck <speckworks@gmail.com>
2019-08-19 20:00:42 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,5 +12,5 @@ def third_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
-   }
-end
+   }}
+ end
